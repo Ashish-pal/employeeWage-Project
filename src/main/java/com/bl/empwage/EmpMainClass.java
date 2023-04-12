7087616ffad71a6b6f43dcd2f363adf42afa257f
Ashish Pal <ak987982@gmail.com>
2023-04-12 16:46:59 +0530

    Added UC2 Calculate Daily Employee Wage

--- src/main/java/com/bl/empwage/EmpMainClass.java
+++ src/main/java/com/bl/empwage/EmpMainClass.java
@@ -1,12 +1,18 @@
 package com.bl.empwage;
 
 public class EmpMainClass {
+    public static final int employeWage_per_hour = 20;
+    public static final int employeWage_full_day = 8;
     public static void main(String[] args) {
         System.out.println("Welcome to employee wage project");
 
         int attendance = (int) (Math.random() * 10) % 2;
-        if (attendance == 1)
+        if (attendance == 1) {
             System.out.println("Employee is present");
+            int wage;
+            wage = employeWage_per_hour * employeWage_full_day;
+            System.out.println(wage);
+        }
         else
             System.out.println("Employee is absent");
     }
