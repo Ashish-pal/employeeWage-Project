a119cc42d87d5b2c04d6a25e1c088d5cd83c473b
Ashish Pal <ak987982@gmail.com>
2023-04-12 17:46:10 +0530

    Added UC4 solving with switch case statement

--- src/main/java/com/bl/empwage/EmpMainClass.java
+++ src/main/java/com/bl/empwage/EmpMainClass.java
@@ -1,18 +1,34 @@
 package com.bl.empwage;
 
+import java.util.Scanner;
+
 public class EmpMainClass {
     public static final int employeWage_per_hour = 20;
     public static final int employeWage_full_day = 8;
-    public static final int employeWage_PART_TIME_HOURS=8;
+    public static final int employeWage_PART_TIME_HOURS=4;
     public static void main(String[] args) {
         System.out.println("Welcome to employee wage project");
-
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter part time hours employee works: ");
+        int part_time = sc.nextInt();
         int attendance = (int) (Math.random() * 10) % 2;
         if (attendance == 1) {
             System.out.println("Employee is present");
             int wage;
             wage = employeWage_per_hour * employeWage_full_day;
             System.out.println(wage);
+            switch (part_time){
+                case employeWage_full_day:
+                    wage = employeWage_per_hour * employeWage_full_day;
+                    System.out.println(wage);
+                    break;
+                case employeWage_PART_TIME_HOURS:
+                    wage = employeWage_per_hour * employeWage_PART_TIME_HOURS;
+                    System.out.println(wage);
+                    break;
+                default:
+                    System.out.println("Error: Invalid number of hours worked");
+            }
         }
         else
             System.out.println("Employee is absent");
